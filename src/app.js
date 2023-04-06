acef574c7c9544113d226c2d6295d32e2d7b361b
Erica G <garceterica@outlook.com>
2023-04-06 20:01:41 -0300

    Add Mongoose and setting

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
+//Import de packetes
 import express from "express";
-import { puerto } from "./config/index.js";
+import mongoose from "mongoose";
 
+//se importan los 2 entornos
+import { puerto } from "./config/index.js";
+import { uri } from "./config/index.js"
+
+//levanta express
 const app = express();
 
+//se solicita a express que nos responda que esta vivo
 app.get('/', (request, response, error ) => {
-  response.send('status: OK');
+  response.send('API levantada!');
 });
 
+//mongoose se conecta al cluster, luego avisa que conecto... sino agarra el error y lo muestra en consola
+mongoose.connect(uri)
+.then(()=> console.log("Conectado a MongoDB Atlas - Cluster Remoto :) "))
+.catch((error)=>console.error(error));
+
+//se le avisa al dev cual es el puerto de escucha en su pc
 app.listen(puerto, () => {
-  console.log(`Server listening in port: ${puerto}`);
+  console.log(`Escuchando en el puerto local: ${puerto}`);
 });
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
+//import de packete dotenv
 import * as dotenv from 'dotenv';
 
+//configuracion de dotenv
 dotenv.config();
 
+//se crean las cosntantes y se exportan a app.js
 export const puerto = process.env.PORT;
+export const uri = process.env.MONGODB_URI;
